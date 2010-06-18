0e0f5fd53fd5dfbc1fbe49a9d6146775f978f8d3
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2010-06-18 11:06:00 +0200

    robustify autodeps
    ignore save and load commands which are not 'plain'

--- autodeps.R
+++ autodeps.R
@@ -7,11 +7,15 @@ isIt <- function(e, what) as.character(e[[1]])==what
 isSave <- function(e) isIt(e, "save")
 isLoad <- function(e) isIt(e, "load")
 
-whatIsSaved <- sapply(Filter(isSave, parsedFile), function(x) as.list(x)$file)
+tryEval <- function(expr) tryCatch(eval(expr), error=function(ignore) NULL)
+
+whatIsSaved <- lapply(Filter(isSave, parsedFile), function(x) tryEval(as.list(x)$file))
+whatIsSaved <- unlist(Filter(is.character, whatIsSaved))
 if(length(whatIsSaved)==0)
   quit("no")
 
-whatIsLoaded <- sapply(Filter(isLoad, parsedFile), "[[", 2)
+whatIsLoaded <- lapply(Filter(isLoad, parsedFile), function(x) tryEval(x[[2]]))
+whatIsLoaded <- unlist(Filter(is.character, whatIsLoaded))
 
 ##genearate Makefile targets
 cat(paste(whatIsSaved, collapse=" "), ": ", fileName, " ",
--- bau/analysis.R
+++ bau/analysis.R
@@ -5,3 +5,16 @@ x <- 1:3
 
 save(x, file="analysis.RData")
 
+save(x, file=paste("an", 1, ".RData", sep=""))
+
+if(FALSE) {
+  save(file="maramao") ## ignored
+}
+
+save(x, file="what"+"is"+"that?") ## this is ignored too
+
+## these are ignored too:
+load(marameo)
+load(123)
+v <- "123"
+load(v)
