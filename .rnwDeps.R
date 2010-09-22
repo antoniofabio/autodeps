cf68641f61c6dd17a964c1a81a0237d2659babb9
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2010-09-22 11:11:45 +0200

    improve rnwDeps script
    rm temp .R file, fix generated dep. pattern

--- .rnwDeps.R
+++ .rnwDeps.R
@@ -7,7 +7,9 @@ fileTex <- paste(dropExt(basename(fileName)), ".tex", sep="")
 fileR <- paste(dropExt(basename(fileName)), ".R", sep="")
 Stangle(fileName)
 output <- system(paste("Rscript .autodeps.R", fileR), intern=TRUE)
+unlink(fileR)
 output <- output[length(output)] #discard eventual warning messages etc.
+output <- gsub("(.*: )(.+?\\.R)(.*)", "\\1\\2nw\\3", output)
 output <- paste(fileTex, output, "\n", sep="")
 sink()
 
